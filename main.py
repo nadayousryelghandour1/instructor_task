c21ec50a66933e5c809240a53793bb4ecad3bd5e
Nada_Yousry_ELghandour <starstrayskz@gmail.com>
2026-09-05 23:51:21 +0200

    feat : Add tenant repository and working /tenants endpoint

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
+from sqlalchemy.orm import sessionmaker
+from infrastructure.database import engine
+from infrastructure.tenant_repository import TenantRepository
 
 app = FastAPI()
+
+Session = sessionmaker(bind=engine)
 
 @app.get("/")
 def read_root():
     return {"message": "Domain Copilot is alive!"}
+
+@app.get("/tenants")
+def get_tenants():
+    session = Session()
+    repository = TenantRepository(session)
+    tenants = repository.get_all()
+    return tenants
